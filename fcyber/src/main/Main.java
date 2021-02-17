ea45fc65913f42d2b3371fd3c2edc924ddaff546
Ömer Faruk Koç <77229286+Fcyb3r@users.noreply.github.com>
2021-02-17 12:00:08 +0300

    piSayisi

---
+++ fcyber/src/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		 //Math.PI, bize PI sayısını döndürür
+        float PI_float  =(float) Math.PI;
+      
+
+        System.out.println("PI sayisi: "+PI_float);
+    
+	}
+
+}
